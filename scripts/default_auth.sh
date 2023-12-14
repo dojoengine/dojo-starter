c356ec3143bb2c22bda97ca3c2d6b39c3a07554b
Frost <froststarbook@gmail.com>
2023-12-14 13:12:52 +0800

    fix issue sozo auth

--- scripts/default_auth.sh
+++ scripts/default_auth.sh
@@ -6,7 +6,7 @@ export RPC_URL="http://localhost:5050";
 
 export WORLD_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.world.address')
 
-export ACTIONS_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.contracts[] | select(.name == "actions" ).address')
+export ACTIONS_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.contracts[] | select(.name == "dojo_examples::actions::actions" ).address')
 
 echo "---------------------------------------------------------------------------"
 echo world : $WORLD_ADDRESS 
@@ -18,7 +18,7 @@ echo "--------------------------------------------------------------------------
 COMPONENTS=("Position" "Moves" )
 
 for component in ${COMPONENTS[@]}; do
-    sozo auth writer $component $ACTIONS_ADDRESS --world $WORLD_ADDRESS --rpc-url $RPC_URL
+    sozo auth writer --world $WORLD_ADDRESS --rpc-url $RPC_URL $component $ACTIONS_ADDRESS 
 done
 
 echo "Default authorizations have been successfully set."
