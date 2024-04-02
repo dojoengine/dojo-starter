2ce7f843e6e2761ed742d178ae6911b81a18448f
Satyam Bansal <satyam@passageprotocol.com>
2024-04-02 16:44:19 +0530

    Fix manifest path in auth and spawn scripts

--- scripts/default_auth.sh
+++ scripts/default_auth.sh
@@ -4,20 +4,20 @@ pushd $(dirname "$0")/..
 
 export RPC_URL="http://localhost:5050";
 
-export WORLD_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.world.address')
+export WORLD_ADDRESS=$(cat ./manifests/deployments/KATANA.json | jq -r '.world.address')
 
-export ACTIONS_ADDRESS=$(cat ./target/dev/manifest.json | jq -r '.contracts[] | select(.name == "dojo_starter::systems::actions::actions" ).address')
+export ACTIONS_ADDRESS=$(cat ./manifests/deployments/KATANA.json | jq -r '.contracts[] | select(.name == "dojo_starter::systems::actions::actions" ).address')
 
 echo "---------------------------------------------------------------------------"
-echo world : $WORLD_ADDRESS 
+echo world : $WORLD_ADDRESS
 echo " "
 echo actions : $ACTIONS_ADDRESS
 echo "---------------------------------------------------------------------------"
 
 # enable system -> models authorizations
 sozo auth grant --world $WORLD_ADDRESS --wait writer \
- Position,$ACTIONS_ADDRESS \
- Moves,$ACTIONS_ADDRESS \
- > /dev/null
+  Position,$ACTIONS_ADDRESS \
+  Moves,$ACTIONS_ADDRESS \
+  > /dev/null
 
 echo "Default authorizations have been successfully set."
